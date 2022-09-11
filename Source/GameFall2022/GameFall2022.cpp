1dff1b2d81e778542157e51d196a382f71ae3e71
brendanehrman1 <brendanehrman@yahoo.com>
2022-09-10 21:01:08 -0500

    Initial commit

---
+++ Source/GameFall2022/GameFall2022.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GameFall2022.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GameFall2022, "GameFall2022" );
