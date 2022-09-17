5d9543c389b14136e6c2b5c13e023597f1ef4060
Bugses <112347348+Bugses@users.noreply.github.com>
2022-09-17 15:22:34 -0500

    added the things

--- Source/GameFall2022/GameFall2022.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "GameFall2022.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GameFall2022, "GameFall2022" );
